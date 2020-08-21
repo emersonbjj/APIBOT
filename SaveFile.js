b919deace743e87fa2518185aeee0032926c0bf2
Jonas Emerson <jonasemerson@outlook.pt>
2020-08-21 20:47:33 -0300

    Caminho para executar automação corrigido

--- SaveFile.js
+++ SaveFile.js
@@ -10,7 +10,7 @@ module.exports = {
                 const contentString = "cd C:/Program Files/Blue Prism Limited/Blue Prism Automate/ \n"
                     + `start AutomateC.exe /run ${process} /resource ${resource} /user admin Ee!41526388 /dbconname Blueprism`;
                 const path = `C:/Users/jonas/Desktop/API_Ajustado_V0.2/Files_Executions/${process}.bat`;
-                const PathRelative = `/Files_Executions/${process}.bat`;
+                const PathRelative = `C:/Users/jonas/Desktop/API_Ajustado_V0.2/Files_Executions/${process}.bat`;
                 fs.writeFileSync(PathRelative, contentString)
                 resolve(PathRelative);
             }
